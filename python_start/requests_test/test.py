a8e312b2853cb7224923d7e0a2d31ef57f71247a
Anwma <51366315+Anwma@users.noreply.github.com>
2022-08-01 23:53:23 +0800

    22-08-01

--- python_start/requests_test/test.py
+++ python_start/requests_test/test.py
@@ -6,8 +6,39 @@ import requests
 # })
 # print(rsp.text)
 
-rsp = requests.post("http://127.0.0.1:8083/post?id=1&page=2", data={
-    "name": "你好",
-    "message": "jeff"
+# rsp = requests.post("http://127.0.0.1:8083/post?id=1&page=2", data={
+#     "name": "你好",
+#     "message": "jeff"
+# })
+# print(rsp.text)
+# from requests_test.proto import user_pb2, user_pb2_grpc
+#
+# user = user_pb2.Teacher()
+#
+# rsp = requests.get("http://127.0.0.1:8083/someProtoBuf")
+# user.ParseFromString(rsp.content)
+# print(user.name, user.course, )
+
+import requests
+
+# 登录
+# rsp = requests.post("http://127.0.0.1:8083/loginJSON", json={
+#     "user": "b",
+#     "password": "imooc",
+# })
+# print(rsp.text)
+
+# 注册
+# rsp = requests.post("http://127.0.0.1:8083/signup", json={
+#     "age": 18,
+#     "name": "bobby",
+#     "email": "13@qq.com",
+#     "password": "imooc",
+#     "rePassword": "imoocc",
+# })
+# print(rsp.text)
+
+rsp = requests.get("http://127.0.0.1:8083/ping", headers={
+    "x-token": "bobbdy"
 })
 print(rsp.text)
